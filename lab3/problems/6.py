58bfb22c54aa8de444917e9f581254bddff68104
Aizere <mirataizere2006@gmail.com>
2024-02-04 02:41:33 +0600

    functions

--- lab3/problems/6.py
+++ lab3/problems/6.py
@@ -1,13 +1,13 @@
-
+firstList = [1, "one", 1.000, 2]
+secondList = [2.0, "one", 1]
 
 def find_common_elements(firstList, secondList):
-    answer = []
-    for x in firstList:
-        for y in secondList:
-            if x == y:
-                answer.append(x)
-    return answer            
-firstList = [1, "one", 1.0]
-secondList = [2.0, "one", 1]
-k = find_common_elements(firstList, secondList)
-print(k)
+    newList = []
+    for element in firstList:
+        if element in secondList:
+            newList.append(element)
+    return newList
+ 
+
+answer = find_common_elements(firstList, secondList)
+print(answer)
