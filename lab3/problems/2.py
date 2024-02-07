73ad6f11f96d2a2c38e6bb9c80145225da5f9962
Aizere <mirataizere2006@gmail.com>
2024-02-07 21:22:00 +0600

    problems

--- lab3/problems/2.py
+++
@@ -1,7 +0,0 @@
-def reverse_string(n):
-    return n[::-1]
-
-n = input()
-
-answer = reverse_string(n)
-print(answer)
--- lab3/problems/3.py
+++
@@ -1,8 +0,0 @@
-import math
-def  get_max(a, b, c):
-    return max(a,b,c)
-
-a, b, c = int(input()), int(input()), int(input())
-
-answer = get_max(a, b, c)  
-print(answer)
